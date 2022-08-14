47de584caa23d1f458a5a67535ee186d73635cd6
Nome Sobrenome <eumarcosborgs@gmail.com>
2022-08-13 21:10:55 -0300

    fix: ContactController

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 require('express-async-errors');
 
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
-app.use(express.json());
-app.use(routes);
-app.use((error, request, response, next) => {
-  console.log('##### Error Handler');
-  console.log(error);
-  response.sendStatus(500);
-});
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+app.use(express.json());
+app.use(cors);
+app.use(routes);
+app.use(errorHandler);
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
